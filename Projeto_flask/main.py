a8ae6d4031970168eff3bcf19e70f4ad3cee0629
Elmi-tecnologia <92441739+Elmi-tecnologia@users.noreply.github.com>
2021-10-14 04:53:03 -0300

    Update main.py

--- Projeto_flask/main.py
+++ Projeto_flask/main.py
@@ -1,8 +1,8 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/index')
-def index():
+@app.route('/index') #rota, o que será digitado na url do navegador para ter acesso a página
+def index(): 
     return '<h1>página index</h1> <br> <p>Conteúdo da página</p>'
 
 @app.route('/login')
@@ -18,4 +18,4 @@ def form():
     return '<h3>Página Formuário</h3> <br> <p><b>Conteúdo da página</b></p> <form> <input type="text" id="nome" /> <label for="nome">Nome:</label>  </form>'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port = 8000)
+    app.run(host='0.0.0.0', port = 8000) #endereço e porta onde está rodando o servidor.
